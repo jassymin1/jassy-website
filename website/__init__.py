2977c41503b13af0905317096edd5d32b5575bed
jassymin1 <colitengj2@winthrop.edu>
2024-05-29 22:18:56 -0400

    more codes added

--- website/__init__.py
+++ website/__init__.py
@@ -8,4 +8,14 @@ def create_app():
     # encrypt/secure session data related to website
     app.config['SECRET_KEY'] = 'noey'
 
+    # import blueprints
+    from .views import views
+    from .auth import auth
+
+    # register blueprints with flask application
+    # url_prefix is saying: "all of the URLs stored inside the blueprints file
+    # how do I access them?"
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 # store standard routes for website
 # where users can actually go to/navigate (ex: home page, anything not related
 # to authentication)
+# NEED to register blueprints to __init__.py
 from flask import Blueprint
 
 # define this file as a blueprint for the flask application
@@ -8,3 +9,11 @@ from flask import Blueprint
 
 # define the name of the blueprint
 views = Blueprint('views', __name__)
+
+
+# to define a view in flask = @nameofblueprint
+# inside the function/bracket put URL to get to this end-point/route
+@views.route('/')
+# define a function
+def home(): # function will run whenever we go to our URL & type in '/' to go to main page of website (whatever is inside of home if gonna run)
+    return "<h1>Test</h1>"
